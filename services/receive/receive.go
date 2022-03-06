3bdecd79d5a79735693ee5c0572a0abd130152bf
Yume <yume.network@protonmail.com>
2022-03-06 17:13:35 +0100

    Mongo

--- services/receive/receive.go
+++ services/receive/receive.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"go.mongodb.org/mongo-driver/bson"
 	"log"
 )
 
@@ -11,7 +13,14 @@ func failOnError(err error, msg string) {
 	}
 }
 
+var mg MongoInstance
+
 func main() {
+	err := Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer func(conn *amqp.Connection) {
@@ -76,6 +85,14 @@ func main() {
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
+			collection := mg.Db.Collection("error")
+			_, err := collection.InsertOne(context.TODO(), bson.D{{
+				Key:   "name",
+				Value: "Chris",
+			}})
+			if err != nil {
+				return
+			}
 		}
 	}()
 
